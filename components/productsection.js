338c23dd92bfe05303c4e8e6da579808fcce07e6
Caertos <cocheroc@gmail.com>
2025-02-23 19:01:01 -0500

    solucion de errores

--- components/productsection.js
+++ components/productsection.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const products = [
     { name: "Samsung Galaxy S21", category: "celulares", price: 2200000, img: "images/SamsungS21.png", },
     { name: "Redmi Note 13S", category: "accesorios", price: 1200000, img: "images/redmi13pro.jpg" },
-    { name: "Producto 3", category: "cargadores", price: 30 },
-    { name: "Producto 4", category: "cargadores", price: 30 },
-    { name: "Producto 5", category: "cargadores", price: 30 },
-    { name: "Producto 6", category: "cargadores", price: 30 },
+    { name: "Producto 3", category: "cargadores", price: 30, img: "images/redmi13pro.jpg" },
+    { name: "Producto 4", category: "cargadores", price: 30, img: "images/redmi13pro.jpg" },
+    { name: "Producto 5", category: "cargadores", price: 30, img: "images/redmi13pro.jpg" },
+    { name: "Producto 6", category: "cargadores", price: 30, img: "images/redmi13pro.jpg" },
   ];
 
   const templateProduct = document.querySelector("#product-template");
@@ -19,7 +19,5 @@ document.addEventListener("DOMContentLoaded", () => {
     productTemplateClone.querySelector(".phoneImg").src = product.img;
     productTemplateClone.querySelector(".phoneImg").alt = product.name;
     data.appendChild(productTemplateClone);
-
-    console.log(data);
   });
 });
