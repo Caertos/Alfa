9aede497e4ced4640c84eae21811140df9ae7b73
Caertos <cocheroc@gmail.com>
2025-03-03 23:26:49 -0500

    agregada maquetación productos

--- components/productsection.js
+++ components/productsection.js
@@ -50,7 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   ];
 
-  const templateProduct = document.querySelector("#product-template");
   const data = document.querySelector(".productData");
   const pagination = document.querySelector("#pagination");
   const itemsPerPage = 6;
@@ -63,14 +62,56 @@ document.addEventListener("DOMContentLoaded", () => {
     const paginatedItems = products.slice(start, end);
 
     paginatedItems.forEach((product) => {
-      const productTemplateClone = templateProduct.content.cloneNode(true);
-      productTemplateClone.querySelector("#prompt").textContent = product.name;
-      productTemplateClone.querySelector("#prompt").href = "#";
-      productTemplateClone.querySelector(".priceProduct").textContent =
-        product.price;
-      productTemplateClone.querySelector(".phoneImg").src = product.img;
-      productTemplateClone.querySelector(".phoneImg").alt = product.name;
-      data.appendChild(productTemplateClone);
+      const div = document.createElement("div");
+      div.className = "tarjeta col-lg-2 col-md-4 col-sm-12 my-2";
+      div.innerHTML = `
+      <div class="containerProduct noselect">
+        <div class="canvas">
+          <div class="tracker tr-1"></div>
+          <div class="tracker tr-2"></div>
+          <div class="tracker tr-3"></div>
+          <div class="tracker tr-4"></div>
+          <div class="tracker tr-5"></div>
+          <div class="tracker tr-6"></div>
+          <div class="tracker tr-7"></div>
+          <div class="tracker tr-8"></div>
+          <div class="tracker tr-9"></div>
+          <div id="card">
+            <div class="card-content">
+              <div class="card-glare"></div>
+              <div class="cyber-lines">
+                <span></span><span></span><span></span><span></span>
+              </div>
+                <a id="prompt" href="#">${product.name}</a>
+              <div class="title d-flex justify-content-center">
+                <img
+                  src="${product.img}"
+                  alt="${product.name}"
+                  class="phoneImg"
+                />
+              </div>
+              <div class="glowing-elements">
+                <div class="glow-1"></div>
+                <div class="glow-2"></div>
+                <div class="glow-3"></div>
+              </div>
+              <div class="subtitle">
+                <span>$</span><span class="highlight priceProduct">${product.price}</span>
+              </div>
+              <div class="card-particles">
+                <span></span><span></span><span></span> <span></span
+                ><span></span><span></span>
+              </div>
+              <div class="corner-elements">
+                <span></span><span></span><span></span><span></span>
+              </div>
+              <div class="scan-line"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+`;
+      data.appendChild(div);
     });
 
     renderPagination();
@@ -132,7 +173,5 @@ document.addEventListener("DOMContentLoaded", () => {
       pagination.appendChild(createPageButton(">", currentPage + 1));
     }
   }
-  console.log("Elemento pagination:", pagination);
   renderProducts();
-  console.log("Botones de paginación generados: ", pagination.innerHTML);
 });
